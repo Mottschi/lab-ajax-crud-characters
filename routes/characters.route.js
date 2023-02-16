956c6c53c2f677a7b3c9e1d916354226bc6d1354
Sebastian Mottschall <srophkor@gmail.com>
2023-02-16 16:24:01 +0100

    iteration 1

--- routes/characters.route.js
+++ routes/characters.route.js
@@ -7,39 +7,86 @@ const Character = require('../models/Character.model')
 /**
  * ? This route should respond with all the characters
  */
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	/**Your code goes here */
+	try {
+		const allCharacters = await Character.find();
+		res.status(200).json(allCharacters);
+	} catch (error) {
+		next(error)
+	}
 })
 
 /**
  * ? This route should create one character and respond with
  * ? the created character
  */
-router.post('/', (req, res, next) => {
-	/**Your code goes here */
+router.post('/', async (req, res, next) => {
+	try {
+		const { name, occupation, cartoon, weapon } = req.body;
+
+		const errorMessages = [];
+		if (!name) errorMessages.push('name');
+		if (!occupation) errorMessages.push('occupation');
+		if (!cartoon || typeof cartoon !== boolean) errorMessages.push('cartoon');
+		if (!weapon) errorMessages.push('weapon');
+		if (errorMessages.length) return res.send(errorMessages);
+
+		const newCharacter = { name, occupation, cartoon, weapon };
+		const createdCharacter = await Character.create(newCharacter);
+		res.status(201).json(createdCharacter);
+	} catch (error) {
+		next(error);
+	}
+	
 })
 
 /**
  * ? This route should respond with one character
  */
-router.get('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.get('/:id', async (req, res, next) => {
+	try {
+		const character = await Character.findById(req.params.id);
+		console.log(character);
+		res.status(200).json(character);
+	} catch (error) {
+		next(error)
+	}
 })
 
 /**
  * ? This route should update a character and respond with
  * ? the updated character
  */
-router.patch('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.patch('/:id', async (req, res, next) => {
+	try {
+		const { name, occupation, cartoon, weapon } = req.body;
+		const updatedCharacterData = {};
+		if (name) updatedCharacterData.name = name;
+		if (occupation) updatedCharacterData.occupation = occupation;
+		if (cartoon) updatedCharacterData.cartoon = cartoon;
+		if (weapon) updatedCharacterData.weapon = weapon;
+
+		const updatedCharacter = await Character.findByIdAndUpdate(req.params.id, updatedCharacterData, {new: true});
+		if (updatedCharacter) res.json(updatedCharacter);
+		else res.send('Character not found')
+	} catch (error) {
+		console.log(error)
+	}
 })
 
 /**
  * ? Should delete a character and respond with a success or
  * ? error message
  */
-router.delete('/:id', (req, res, next) => {
-	/**Your code goes here */
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const deletedChar = await Character.findByIdAndDelete(req.params.id);
+		if (deletedChar) res.send("Character has been successfully deleted");
+		else res.send("Character not found");
+	} catch (error) {
+		next(error);
+	}
 })
 
 module.exports = router
